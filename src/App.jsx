345e680a4fc38599319b469709ad391225d8dcb8
Irenity0 <lilithkard77@gmail.com>
2026-05-16 11:39:32 +0300

    added zustand & clsx to handle 3d Product model color & size state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import ProductViewer from "./components/ProductViewer"
 
 const App = () => {
   return (
     <>
     <Navbar />
     <Hero/>
+    <ProductViewer/>
     </>
   )
 }
